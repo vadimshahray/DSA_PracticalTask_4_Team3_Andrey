163e393993ec1204424c6709555e59dfd1ea9c84
Вадим Шахрай <vadimshahray.workmail@gmail.com>
2022-05-29 22:48:13 +0700

    бульбуль

--- DSA_PracticalTask_4_Team3_Andrey/table.cpp
+++ DSA_PracticalTask_4_Team3_Andrey/table.cpp
@@ -4,28 +4,36 @@
 #include "table.h"
 #include "product.h"
 
+bool table::index_of(product *item, ULONGLONG &i)
+{
+   i = 0;
+   for (bool equals = true; i < size; i++) // Поиск элемента с таким же ключом
+   {
+      equals = true;
+      for (char j = 0; j < sizeof data[i]->code && equals; j++)
+         equals = data[i]->code[j] == item->code[j];
+
+      if (equals)
+         return true;
+   }
+   return false;
+}
+
 product *table::insert_or_assign(product *item)
 {
    if (size == Nmax)
       return NULL;
 
-   ULONGLONG i = 0;
-   bool equals = false; // Нашелся ли элемент с таким же ключом
-   
-   for ( ; i < size && !equals; i++) // Поиск элемента с таким же ключом
-   {
-      equals = true;
-      for (char j = 0; j < sizeof data[i]->code && equals; j++)
-         equals = data[i]->code[j] == item->code[j];
-   }
 
-   if (!equals)
+   ULONGLONG i = 0;
+   if (!this->index_of(item, i))
    {
       data[size++] = item;
       return NULL;
    }
-   else return data[i - 1];
+   else return data[i];
 }
+
 
 void table::input(FILE *f)
 {
@@ -38,7 +46,7 @@ void table::input(FILE *f)
       fscanf_s(f, "%lli", &p->amount);
 
       product *fp = this->insert_or_assign(p);
-      if (fp) 
+      if (fp)
          fp->amount += p->amount;
 
    } while (fscanf_s(f, "%c", &c, (UINT) sizeof c) != EOF);
--- DSA_PracticalTask_4_Team3_Andrey/table.h
+++ DSA_PracticalTask_4_Team3_Andrey/table.h
@@ -13,19 +13,15 @@ struct table
    ULONGLONG size = 0;
    product *data[Nmax] = { };
 
+   bool index_of(product *item, ULONGLONG &i);
+
    public:
-   /// <summary>
-   /// Добавляет новый элемент item в таблицу или возвращает указатель на старый, если он уже есть в таблице
-   /// </summary>
+   // Добавляет новый элемент item в таблицу или возвращает указатель на старый, если он уже есть в таблице
    product *insert_or_assign(product *item);
 
-   /// <summary>
-   /// Выводит содержимое таблицы на консоль
-   /// </summary>
+   // Выводит содержимое таблицы на консоль
    void print();
-   /// <summary>
-   /// Строит таблицу по содержимому файла f
-   /// </summary>
+   // Строит таблицу по содержимому файла f
    void input(FILE *f);
 };
 #endif
